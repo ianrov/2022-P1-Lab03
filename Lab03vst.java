268a4acbda6f78f36937962082ec0456890f9d54
11645 <ian.rovner@vtsd.com>
2022-09-19 10:02:25 -0400

    this was bad

--- Lab03vst.java
+++ Lab03vst.java
@@ -8,7 +8,24 @@ public class Lab03vst
 {
 	public static void main(String[] args)
 	{
-      System.out.println("Lab03, 80 Point Version\n");
+      System.out.println("Lab03, 100 Point Version\n");
+
+	  int ms=10000123;
+	  System.out.println("Starting Milli-Seconds:"+ms);
+
+	  int h=ms/3600000;
+	  System.out.println("Hours:"+h);
+
+	  int r= ms%3600000;
+	  int m=r/60000;
+	  System.out.println("Minutes:"+m);
+
+	  int rm= r % 60000;
+	  int s=rm/1000;
+	  System.out.println("Seconds:"+s);
+
+	  int rms=rm%1000;
+	  System.out.println("Milli-Seconds:"+rms);
       
 	}
 }
